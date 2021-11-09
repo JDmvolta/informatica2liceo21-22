66df2d6f07fac60f275ef2767398f36112fbb142
JDmvolta <92609786+JDmvolta@users.noreply.github.com>
2021-11-08 20:54:25 -0500

    Add files via upload

---
+++ javascrpt/index.js
@@ -0,0 +1,7 @@
+var numero = prompt("elija un número de su preferencia")
+var numero2 = prompt("elija otro número")
+var sumatoria = numero + numero2
+alert("multiplicación="+( numero * numero2))
+alert("resta="+( numero - numero2))
+alert("división="+(numero / numero2))
+alert("suma="+ sumatoria)
