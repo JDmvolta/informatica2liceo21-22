811fba0d4a9745ce4afa04e3c4089e60909f795f
JDmvolta <92609786+JDmvolta@users.noreply.github.com>
2022-02-01 15:24:54 -0500

    Update mario1.js

--- mario1.js
+++ mario1.js
@@ -1,15 +1,24 @@
-function piso(nPiso, altura){
-nEspacios=altura-nPiso;
-nNumerales=nPisos;
-texto="<p>"
-for(let i=0; i<nEspacios; i++)
-{
-    texto=texto+"&nbsp"
+function piso(nPiso, altura){ 
+    //To do. Deben poner el número correcto
+    // de espacios y de numerales de acuerdo
+    // al Npiso y a la altura 
+    return  "<p>" + "####" + "</p>"
+}
+let nPisos;
+do{
+//lo que se hace : recibir el número
+//del usuario y guardarlo como número
+nPisos = parseInt(prompt("Ingrese la altura de la torre. Número entre 1 y 10"))
+}while(isNaN(nPisos) || nPisos < 1 ||  nPisos> 10);// La condición para que se repita
+let contenido = "";
+for(let i = 1; i< nPisos; i ++){ 
+    contenido = contenido + piso(i, nPisos)
+}
 
-}
-for(let i=0; i<nNumerales; i++)
-{
-    texto=texto + "#"
-}
-texto=texto + "</p>";
-return texto;
+let contenido =  `<p> &nbsp;&nbsp; &nbsp; &nbsp;<s</p>
+<p> &nbsp; &nbsp; ec</p>
+<p> &nbsp;ond</p>
+<p>a  <li</p>`
+let contenedor = document.querySelector("#contenedor");
+contenedor.innerHTML = contenido;
+
